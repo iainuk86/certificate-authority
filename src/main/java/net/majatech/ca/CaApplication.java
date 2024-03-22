dee6b0a30224bcaab78cfaf3bf56874b9c693120
iainuk86 <iainuk1986@gmail.com>
2024-03-22 13:18:31 +1100

    Enable JPA Repositories

--- src/main/java/net/majatech/ca/CaApplication.java
+++ src/main/java/net/majatech/ca/CaApplication.java
@@ -2,12 +2,12 @@ package net.majatech.ca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "net.majatech.ca.data.repo")
 public class CaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CaApplication.class, args);
     }
-
 }
